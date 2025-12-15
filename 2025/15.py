3bafe5a31330814c25ae98c66d922e06455ed4a3
Hugo Fuentes <fuentes9hugo@gmail.com>
2025-12-15 20:58:41 +0100

    Exercise 15 - 2025

--- 2025/15.py
+++ 2025/15.py
@@ -15,8 +15,48 @@ Mira el ejemplo para ver cómo debes dibujar la tabla:"""
 
 
 def drawTable(data: list[dict[str, str | int]], sortBy: str) -> str:
-    # Code here
-    return ""
+    data_map = [tuple(value for value in row.values()) for row in data]
+    
+    sort_by_index = tuple(data[0].keys()).index(sortBy)
+    data_map.sort(key=lambda x: x[sort_by_index])
+
+    column_width = []
+    for row in data_map:
+        for i, col in enumerate(row):
+            if len(column_width) < i + 1: column_width.append(len(str(col)))
+
+            if len(str(col)) > column_width[i]:
+                column_width[i] = len(str(col))
+    
+    column_headers = (chr(i) for i in range(ord("A"), ord("Z") + 1))
+    
+    table_limit = ["+" + "-" * (width + 2) for width in column_width]
+    table_limit.append("+")
+    table_limit = "".join(table_limit)
+
+    table = [table_limit]
+    header_row = ["|"]
+
+    for i, row in enumerate(data_map):
+        table_row = ["|"]
+        for j, col in enumerate(row):
+            if i == 0:
+                header = next(column_headers)
+                header_row.append(" " + header + " " * (column_width[j] - len(header)) + " |")
+                if j == len(row) - 1:
+                    header_row = "".join(header_row)
+                    table.append(header_row)
+                    table.append(table_limit)
+
+            table_row.append(" " + str(col) + " " * (column_width[j] - len(str(col))) + " |")
+
+            if j == len(row) - 1:
+                table_row = "".join(table_row)
+                table.append(table_row)
+
+    table.append(table_limit)   
+
+    return "\n".join(table)
 
 
 def main():
