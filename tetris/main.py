fd160e1effbd53f691c3f796c6956db24ae3fd94
Hugo Fuentes <hfuentes@ale-hop.org>
2026-01-20 16:57:50 +0100

    Tetris: clean main

--- tetris/main.py
+++ tetris/main.py
@@ -2,29 +2,7 @@ from models import Board, O, I, T, S, Z, L, J
 
 
 def main():
-    o, i, t, s, z, l, j = O(), I(), T(), S(), Z(), L(), J()
-    print(l)
-    l.rotate("right")
-    print()
-    print(l)
-    print()
-    l.rotate("right")
-    print(l)
-    print()
-    l.rotate("right")
-    print(l)
-    l.rotate("right")
-    print()
-    print(l)
-    print()
-    l.rotate("left")
-    print(l)
-    print()
-    l.rotate("left")
-    print(l)
-    l.rotate("left")
-    print()
-    print(l)
+    pass
 
 if __name__ == "__main__":
     main()
