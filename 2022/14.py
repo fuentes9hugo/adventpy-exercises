f0b4e0f6b10a4ca0d8ce0ad5f4f9ee011abacf5e
Hugo Fuentes <fuentes9hugo@gmail.com>
2026-02-02 23:05:05 +0100

    Exercise 14 - 2022

---
+++ 2022/14.py
@@ -0,0 +1,110 @@
+"""Santa Claus está construyendo pirámides de hielo en el Polo Norte para entrenar a sus renos.
+
+Cada reno comienza en la cima de la pirámide y debe elegir el camino óptimo hacia abajo para recorrerlo en el menor tiempo posible. Cada nivel tiene un número que determina el tiempo que necesita para llegar ahí.
+
+Al llegar a una posición, el reno solo puede deslizarse hacia abajo y en diagonal hacia la izquierda o la derecha. Visualmente, la pirámide se ve así:"""
+
+#     0
+#    / \
+#   7   4
+#  / \ / \
+# 2   4   6
+"""En código la representamos así:
+
+[
+  [0],
+  [7, 4],
+  [2, 4, 6]
+]
+Santa Claus necesita un programa que le dice cuál es el tiempo mínimo que puede tomar cada reno para deslizarse por la pirámide usando el camino más óptimo.
+
+En el ejemplo anterior, el camino más óptimo es 0 -> 4 -> 4, que toma un total de 0 + 4 + 4 = 8 unidades de tiempo. El resultado sería: 8.
+
+¿Por qué no es 6? No es 0 -> 4 -> 2 porque al bajar a la posición del 4 ya no puede diagonalmente llegar a la posición del 2. Así que el camino más corto posible es 8.
+
+A tener en cuenta:
+- Ten en cuenta que sólo puedes bajar en diagonal y hacia abajo desde cualquier posición.
+- La primera posición de la pirámide puede ser diferente a 0.
+- Las pirámides pueden tener hasta 10 niveles.
+- Los números en las pirámides pueden ser negativos."""
+
+
+def getOptimalPath(path: list[list[int]]) -> int:
+    def backtracking(path: list[list[int]], current_ammount: int, index: int) -> int:
+        if len(path) == 1: return current_ammount + min(path[0][index:index + 2])
+
+        ammount = None
+
+        for i, time_needed in enumerate(path[0][index:index + 2], start=index):
+            track = backtracking(path[1:], current_ammount + time_needed, i)
+
+            if ammount == None or track < ammount: ammount = track
+            
+        return ammount
+
+    return backtracking(path[1:], path[0][0], 0) if len(path) > 1 else path[0][0]
+
+
+def test(e, r):
+    return e == r
+
+
+def main():
+    print(test(2, getOptimalPath([[0], [2, 3]]))) # 2
+    # 0 -> 2
+
+    print(test(5, getOptimalPath([[0], [3, 4], [9, 8, 1]]))) # 5
+    # 0 -> 4 -> 1
+
+    print(test(8, getOptimalPath([[1], [1, 5], [7, 5, 8], [9, 4, 1, 3]]))) # 8
+    # 1 -> 1 -> 5 -> 1
+
+    pyramid = [
+            [5],
+            [9, 4],
+            [1, 5, 2],
+            [8, 3, 7, 1],
+            [1, 2, 4, 5, 6],
+            [9, 1, 2, 3, 1, 1],
+            [4, 5, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1],
+            [9, 9, 9, 9, 9, 9, 9, 9, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+        ]
+    
+    print(test(23, getOptimalPath(pyramid)))
+
+    pyramid_2 = [
+            [0],
+            [7, 4],
+            [2, 4, 6]
+        ]
+    
+    print(test(8, getOptimalPath(pyramid_2)))
+
+    pyramid_3 = [
+            [10],
+            [1, 5],
+            [-10, 2, -20]
+        ]
+    
+    print(test(-5, getOptimalPath(pyramid_3)))
+
+    pyramid_4 = [
+            [1],
+            [50, 50],
+            [1, 100, 1],
+            [1, 1, 1, -1000]
+        ]
+    
+    print(test(-948, getOptimalPath(pyramid_4)))
+
+    pyramid_5 = [[1], [1, 1], [1, 1, 1]]
+
+    print(test(3, getOptimalPath(pyramid_5)))
+
+    print(test(10, getOptimalPath([[10]])))
+
+
+if __name__ == "__main__":
+    main()
