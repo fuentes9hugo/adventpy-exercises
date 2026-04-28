2a6d1d2c7f32c066abf50beb680b0e412c4ae914
Hugo Fuentes <fuentes9hugo@gmail.com>
2026-04-28 20:59:09 +0200

    Taxman iterative

--- taxman_iterative.py
+++ taxman_iterative.py
@@ -2,17 +2,26 @@ def taxman(envelopes_amount: int) -> list[int]:
     envelopes = {envelope: {j for j in range(1, envelope) if envelope % j == 0}
                  for envelope in range(2, envelopes_amount + 1)}
     
-    stack = [([], set())]
+    stack = [([], set(), 2)]
 
     solution = []
 
     while stack:
-        current_list, forbiddens = stack.pop()
+        current_list, forbiddens, envelope = stack.pop()
 
-        for envelope, dividers in envelopes.items():
-            if envelope in forbiddens or dividers.issubset(forbiddens): continue
+        if envelope not in envelopes: continue
 
-            
+        divisors = envelopes[envelope]
+
+        stack.append((current_list, forbiddens, envelope + 1))
+
+        if envelope in forbiddens or divisors.issubset(forbiddens): continue
+
+        new_list = current_list + [envelope]
+
+        stack.append((new_list, forbiddens | {envelope} | divisors, 2))
+
+        if sum(new_list) > sum(solution): solution = new_list
 
     return solution
 
